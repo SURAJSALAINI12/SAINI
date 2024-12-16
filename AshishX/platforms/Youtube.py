5964bc1692e89be9a2c514df3ef5da19aef1049f
ASHISHTIME <155893081+BWFTIME@users.noreply.github.com>
2024-12-16 11:47:26 +0530

    Update Youtube.py

--- AshishX/platforms/Youtube.py
+++ AshishX/platforms/Youtube.py
@@ -10,8 +10,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from VenomX.utils.formatters import time_to_seconds
-from VenomX.utils.database import is_on_off
+from asuixxxx.utils.formatters import time_to_seconds
+from asuixxxx.utils.database import is_on_off
 
 
 def cookies():
