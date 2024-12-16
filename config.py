68e64bfa7ffddade58f0834b0f805fc23423a28d
SURAJSALAINI12 <surajrajsaini218@gmail.com>
2024-12-16 14:59:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -71,7 +71,7 @@ STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
-STRING5 = getenv("STRING_SESSION5", None))
+STRING5 = getenv("STRING_SESSION5", None)
 
 
 BANNED_USERS = filters.user(Bwf)
